0b2aa10e420881eef42ddc6b0e3f2fa72d1b3796
Mohd Yasir Hussain <hussainmohdyasir@gmail.com>
2022-02-01 21:58:15 +0530

    Fixed 16-bug-fixing

---
+++ post-install.js
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+const fs = require('fs');
+
+console.log('Do an postinstall');
+console.log('Copy tonclient.wasm to public');
+fs.copyFileSync('./node_modules/@tonclient/lib-web/tonclient.wasm', './public/tonclient.wasm');
+console.log('Copy tonclient.wasm to public. Done');
